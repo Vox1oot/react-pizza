0846a82abf12eb51c069856be7a39a4bc996e035
Vox1oot <voxfoot@gmail.com>
2023-03-29 22:45:56 +0500

    delete fonts scss

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,27 +1,48 @@
 import React from 'react';
+import axios from 'axios';
 import Categories from './Categories';
 import Sort from './Sort';
 import PizzaBlock from './PizzaBlock';
-import pizzas from '../assets/pizzas.json';
 
-const Content = () => (
-    <div className="content">
-        <div className="container">
-            <div className="content__top">
-                <Categories />
-                <Sort />
-            </div>
-            <h2 className="content__title">Все пиццы</h2>
-            <div className="content__items">
-                {pizzas.map((pizza) => (
-                    <PizzaBlock
-                        key={pizza.id}
-                        {...pizza}
-                    />
-                ))}
+interface Ipizza {
+    id: number,
+    imageUrl: string,
+    title: string,
+    types: number[],
+    sizes: number[],
+    price: number,
+    category: number,
+    rating: number
+}
+
+const Content = () => {
+    const [pizzas, setPizzas] = React.useState<Ipizza[]>([]);
+
+    React.useEffect(() => {
+        axios
+            .get('https://642466119e0a30d92b1b018c.mockapi.io/items')
+            .then((response) => setPizzas(response.data));
+    }, []);
+
+    return (
+        <div className="content">
+            <div className="container">
+                <div className="content__top">
+                    <Categories />
+                    <Sort />
+                </div>
+                <h2 className="content__title">Все пиццы</h2>
+                <div className="content__items">
+                    {pizzas.map((pizza) => (
+                        <PizzaBlock
+                            key={pizza.id}
+                            {...pizza}
+                        />
+                    ))}
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default Content;
